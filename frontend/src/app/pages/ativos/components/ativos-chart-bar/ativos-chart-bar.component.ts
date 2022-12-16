4205b3e39b41d1d8b971c98dc46f46965aee46b6
carmoPjunior <carmo.goncalves.jr@hotmail.com>
2022-12-15 21:17:44 -0300

    Implementa o componente char-bar da page ativos

--- frontend/src/app/pages/ativos/components/ativos-chart-bar/ativos-chart-bar.component.ts
+++ frontend/src/app/pages/ativos/components/ativos-chart-bar/ativos-chart-bar.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Input, OnInit } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+import { IAtivo } from '../../../../shared/models/ativo.model';
+import {MesPorExtensoConstants} from '../../../../shared/constants/mesPorExtensoConst'
 
 @Component({
   selector: 'app-ativos-chart-bar',
   templateUrl: './ativos-chart-bar.component.html',
   styleUrls: ['./ativos-chart-bar.component.scss']
 })
-export class AtivosChartBarComponent {
+export class AtivosChartBarComponent implements OnInit{
+
+  @Input() yearSelected!: number;
+  @Input() ativos: IAtivo[] = [];
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    scales: {
+      x: {},
+      y: {min: 0}
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      title: {
+        display: true,
+        text: 'Lucro Total Ativos',
+        padding: {
+            top: 2,
+            bottom: 2
+        }
+    }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: MesPorExtensoConstants.listMesAbreviado,
+    datasets: []
+  };
+
+  ngOnInit(): void {
+
+    let valuesGroupByYear = this.groupByYear(this.ativos, 'ano');
+
+    for (const key in valuesGroupByYear) {
+      this.barChartData.datasets.push(valuesGroupByYear[key])
+    }
+
+  }
+
+  groupByYear (array: any, key: any) {
+    return array.reduce((accumulator: any, currentItem: any) => {
+
+        let newItem = {
+          data:[],
+          label: currentItem.ano
+        }
+
+        if (!accumulator[currentItem[key]])
+          accumulator[currentItem[key]] = newItem
+
+          accumulator[currentItem[key]]['data'].push(currentItem.totalLucroVendaDivendendo)
+
+        return accumulator
+      }, {})
+  }
+
 
 }
