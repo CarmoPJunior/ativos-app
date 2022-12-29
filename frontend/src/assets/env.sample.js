081c466f8428fcd2b755d6d25e50765499705c84
carmoPjunior <carmo.goncalves.jr@hotmail.com>
2022-12-29 01:30:39 -0300

    Corrige configuração do arquivo env.js

--- frontend/src/assets/env.sample.js
+++ frontend/src/assets/env.sample.js
@@ -2,6 +2,6 @@
   window.env = window.env || {};
 
   // Environment variables
-  window['env']['backendBaseUrl'] = "${API_HOST}:${API_PORT}"
+  window['env']['backendBaseUrl'] = "${API_URL}"
   window["env"]["debug"] = "${DEBUG}";
 })(this);
